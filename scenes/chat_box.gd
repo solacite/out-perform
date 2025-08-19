537d085e40b20bdfb0acfee64f58dafdf5ef37b7
solacite <julia.nguyen.do@gmail.com>
2025-08-19 23:40:07 +0700

    typing animation for dialogue

--- scenes/chat_box.gd
+++ scenes/chat_box.gd
@@ -18,12 +18,16 @@ var dialogues = {
 	]
 }
 
-var current_sequence = []
-var current_index = 0
-var dialogue_finished = false
+var current_sequence: Array = []
+var current_index := 0
+var dialogue_finished := false
+var typing := false
+var typing_speed := 0.03
 
 func _ready():
-	print(text_label)
+	text_label.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	mouse_filter = Control.MOUSE_FILTER_STOP
+
 	gui_input.connect(_on_gui_input)
 	start_dialogue("intro")
 
@@ -32,22 +36,37 @@ func start_dialogue(key: String):
 		current_sequence = dialogues[key]
 		current_index = 0
 		dialogue_finished = false
-		show_next_line()
+		call_deferred("show_next_line")
 
 func show_next_line():
 	if current_index < current_sequence.size():
-		text_label.text = current_sequence[current_index]
-		current_index += 1
-
-		if current_index == current_sequence.size():
-			dialogue_finished = true
+		start_typing(current_sequence[current_index])
 	else:
+		dialogue_finished = true
 		SceneTransition.change_scene_to("res://scenes/gameplay.tscn")
 
+func start_typing(line: String) -> void:
+	typing = true
+	text_label.text = ""
+	await get_tree().process_frame
+	for i in range(line.length()):
+		if not typing:
+			text_label.text = line
+			break
+		text_label.text += line[i]
+		await get_tree().create_timer(typing_speed).timeout
+	typing = false
+
+	current_index += 1
+	
+	if current_index >= current_sequence.size():
+		dialogue_finished = true
 
 func _on_gui_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		if dialogue_finished:
+		if typing:
+			typing = false
+		elif dialogue_finished:
 			SceneTransition.change_scene_to("res://scenes/gameplay.tscn")
 		else:
 			show_next_line()
